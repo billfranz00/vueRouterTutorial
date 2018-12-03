ccdb531b146121e0f85b3004cca69f3f8fe2d59b
billyf <Maggie91>
2018-12-03 15:39:49 -0800

    Route params thru router and props

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -4,21 +4,31 @@
 	    <hr>
 	    <p>Loaded ID: {{ id }}</p>
 	    <button @click="topHomePage" class="btn btn-primary">Go To Home</button>
+	    <hr>
+	    <h3>{{ text }}</h3>
     </div>
 </template>
 
 <script type="text/javascript">
 	export default {
-		data() {
-			return {
-				id: this.$route.params.id
+		props: {
+			id: {
+				type: String
+			},
+			text: {
+				type: String
 			}
 		},
-		watch: {
-			'$route'(to, from) {
-				this.id = to.params.id;
-			}
-		},
+		// data() {
+		// 	return {
+		// 		id: this.$route.params.id
+		// 	}
+		// },
+		// watch: {
+		// 	'$route'(to, from) {
+		// 		this.id = to.params.id;
+		// 	}
+		// },
 		methods: {
 			topHomePage() {
 				this.$router.push("/");
--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,7 @@ import Home from './components/Home.vue';
 
 export const routes = [
 	{ path: '', component: Home },
-	{ path: '/user/:id', component: User }
+	// { path: '/user/:id', component: User }
+	{ path: '/user/:id', component: User, props: true } // pass route.params to props
+	// { path: '/user/:id', component: User, props: { text: "Yo Dude!!!" }} // Can pass static, dynamic, or function to props
 ];
