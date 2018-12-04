2d63785bd438ef741ee0a4de43895b02c59375cd
billyf <Maggie91>
2018-12-03 16:53:08 -0800

    more props for routes

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,6 @@ import Home from './components/Home.vue';
 export const routes = [
 	{ path: '', component: Home },
 	// { path: '/user/:id', component: User }
-	{ path: '/user/:id', component: User, props: true } // pass route.params to props
-	// { path: '/user/:id', component: User, props: { text: "Yo Dude!!!" }} // Can pass static, dynamic, or function to props
+	// { path: '/user/:id', component: User, props: true } // pass route.params to props
+	{ path: '/user/:id', component: User, props: { default: true, text: "Yo Dude!!!" }} // Can pass static, dynamic, or function to props
 ];
